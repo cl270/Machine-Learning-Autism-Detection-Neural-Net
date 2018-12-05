9ecc59e021c8bf96acfaf4b532f1cc07b30a8e9e
Paul <cl270@duke.edu>
2018-12-05 15:01:37 -0500

    initial commit

---
+++ src/neuron.java
@@ -0,0 +1,13 @@
+
+public class neuron {
+	private double weight;
+	private double bias;
+	
+	public void changeWeight(double input) {
+		this.weight = input;
+	}
+	
+	public void changeBias(double input) {
+		this.bias = input;
+	}
+}
