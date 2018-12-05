f531d5ffbf44b2b16c463a8db8408e298c83cce2
Paul <cl270@duke.edu>
2018-12-05 15:39:56 -0500

    update neuron

--- src/neuron.java
+++ src/neuron.java
@@ -1,32 +1,30 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class neuron {
 	private double weight;
 	private double bias;
-	private ArrayList inputs;
+	private List<Double> inputs;
 	private double output;
-	
 	
 	neuron(double weight, double bias){
 		this.weight = weight;
 		this.bias = bias;
+		this.output = 0;
+		this.inputs = new ArrayList<Double>();
 	}
 	
-	public void changeWeight(double input) {
-		this.weight = input;
-	}
+	public void changeWeight(double input) {	this.weight = input;	}
 	
-	public void changeBias(double input) {
-		this.bias = input;
-	}
+	public void changeBias(double input) {	 this.bias = input;	}
 	
-	public double getWeight() {
-		return this.weight;
-	}
+	public double getWeight() {	  return this.weight;	}
 	
-	public double getBias() {
-		return this.bias;
-	}
+	public double getBias() {	return this.bias;	}
+	
+	public double getOutputAxon() {   return this.output;	}
+	
+	public void inputsComplete() {	 decideOutput();	}
 	
 	public void addInputDendrite(neuron N) {
 		double w = N.getWeight();
@@ -34,7 +32,14 @@ public class neuron {
 		this.inputs.add(w*a);
 	}
 	
-	public double getOutputAxon() {
-		return this.output;
+	private void decideOutput() {
+		for(int i = 0; i<this.inputs.size(); i++) {
+			this.output += inputs.get(i);
+		}
+		
+		this.output += this.bias;
+		
+		sigmoidFunction s = new sigmoidFunction(this.output);
+		this.output = s.getSig();
 	}
 }
