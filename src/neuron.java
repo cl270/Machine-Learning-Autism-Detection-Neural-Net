6b7ab67db0b8a45967735bce239c408aba1de700
Paul <cl270@duke.edu>
2018-12-05 15:15:02 -0500

    update neuron

--- src/neuron.java
+++ src/neuron.java
@@ -2,6 +2,15 @@
 public class neuron {
 	private double weight;
 	private double bias;
+	private double[] inputs;
+	private double output;
+	
+	
+	neuron(double weight, double bias, int inputsize){
+		this.weight = weight;
+		this.bias = bias;
+		this.inputs = new double[inputsize];
+	}
 	
 	public void changeWeight(double input) {
 		this.weight = input;
